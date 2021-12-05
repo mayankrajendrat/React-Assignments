4d03a72c08bd3f8dd4fab9ba50141bd2d0f1b498
Mayank Tiwari <mayank_tiwari@epam.com>
2021-12-06 02:13:09 +0530

    Pushed Code with Test cases using Enzyme

---
+++ Assignment-2/code/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App(props) {
+return(
+  <div className="container">
+{/* Start code from here */}
+</div>
+)
+}
+
+
+
+export default App;
